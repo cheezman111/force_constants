520d3f85089d8261ba7e2f436a804af383fb9db3
Armando Garcia <armandogarcia11@gmail.com>
2020-07-22 22:39:14 -0600

    fc_generator.py updated.

--- fc_generator.py
+++ fc_generator.py
@@ -10,36 +10,37 @@ import pymatgen as mg
 import fc_matcher as fcm
 import h5py
 
+supercell_size = 4
 
 # Create a pymatgen lattice, lengths are in Angstroms.
 lat = mg.Lattice.cubic(3.5175)
 # Produce a BCC structure with Zr atoms.
 struct = mg.Structure(lat, ['Zr','Zr'], [[0,0,0],[.5,.5,.5]] )
 # Expand to supercell with 16 total atoms.
-struct.make_supercell([2,2,2])
+struct.make_supercell([supercell_size]*3)
 # Create poscar object and write to file.
 pc = mg.io.vasp.inputs.Poscar(struct)
 pc.write_file('POSCAR')
 
 
-# Hardcode force constant matrices for for 0th-5th nearest neighbors using 
+# Hardcode force constant matrices for for 0th-5th nearest neighbors using
 # experimental values from Heiming-1991.
 # NOTE: dyne/cm -> eV/A**2, check zeros
 unit_conv = .000062415
-c0xx = 75848.0*unit_conv
-c1xx =  7798.4*unit_conv
-c1xy =  8341.7*unit_conv
-c2xx =  4960.5*unit_conv
-c2yy = -2170.2*unit_conv
-c3xx =   838.6*unit_conv
-c3zz = -1410.0*unit_conv
-c3xy =   866.9*unit_conv
-c4xx =    45.5*unit_conv
-c4yy =   204.4*unit_conv
-c4yz =  -995.4*unit_conv
-c4xy =  -134.9*unit_conv
-c5xx =   172.1*unit_conv
-c5xy =   703.0*unit_conv
+c0xx =  75848.0*unit_conv
+c1xx =   7798.4*unit_conv
+c1xy =   8341.7*unit_conv
+c2xx =   4960.5*unit_conv
+c2yy =  -2170.2*unit_conv
+c3xx =    838.6*unit_conv
+c3zz =  -1410.0*unit_conv
+c3xy =    866.9*unit_conv
+c4xx =     45.5*unit_conv
+c4yy =    204.4*unit_conv
+c4yz =   -995.4*unit_conv
+c4xy =   -134.9*unit_conv
+c5xx =    172.1*unit_conv
+c5xy =    703.0*unit_conv
 
 # Load force constants as matrices into fc matcher.
 fc = fcm.Force_Constants_BCC()
@@ -61,11 +62,11 @@ fc.add([2,2,0],np.array([[c3xx,c3xy,0.  ],
                          [c3xy,c3xx,0.  ],
                          [0.  ,0.  ,c3zz]]))
 
-'''
+#'''
 fc.add([3,1,1],np.array([[c4xx,c4xy,c4xy],
                          [c4xy,c4yy,c4yz],
                          [c4xy,c4yz,c4yy]]))
-'''
+#'''
 
 fc.add([2,2,2],np.array([[c5xx,c5xy,c5xy],
                          [c5xy,c5xx,c5xy],
@@ -76,7 +77,7 @@ fc.add([2,2,2],np.array([[c5xx,c5xy,c5xy],
 fc.add([3,3,1], fc.gen_fc_matrix([1,1,1]))
 fc.add([3,3,3], fc.gen_fc_matrix([1,1,1]))
 
-fc.add([3,1,1], fc.gen_fc_matrix([1,1,1]))
+#fc.add([3,1,1], fc.gen_fc_matrix([1,1,1]))
 
 
 # Create the force constants tensor. 
@@ -85,22 +86,32 @@ for site1 in struct.sites:
     fc_tensor.append([])
     for site2 in struct.sites:
         # Displ_coord is calcualted as a numpy array.
-        displ_coord = (site2.frac_coords-site1.frac_coords)*4
+        displ_coord = (site2.frac_coords-site1.frac_coords)*2*supercell_size
         # Convert individual elements to list of ints.
         displ_coord = list(map(int, map(round, displ_coord)))
         fc_matrix = fc.gen_fc_matrix(list(displ_coord))
         # If no fc_matrix matches, use zeros.
         if fc_matrix is None:
-            fc_matrix = fc.gen_fc_matrix([0,0,0])
+            fc_matrix = np.zeros((3,3))
         fc_tensor[-1].append(fc_matrix)
 fc_tensor = np.array(fc_tensor)
+
+self_forces = np.sum(fc_tensor, axis=1)
+
+
+'''
+for index in range(fc_tensor.shape[0]):
+    fc_tensor[index, index] = -self_forces[index]
+'''
+
 
 # Write force constants tensor to hdf5 file
 hf = h5py.File("force_constants.hdf5", "w")
 fc_dataset = hf.create_dataset("force_constants", data=fc_tensor)
+hf.close()
 
 
-''' 
+'''
 # OBSOLETE IF USING HDF5 FILE
 # Generate FORCE_CONSTANTS file.
 
